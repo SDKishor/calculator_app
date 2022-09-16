f1825b7cfbde682161228b4785b3b8ad74d5af32
SDKishor <91415355+SDKishor@users.noreply.github.com>
2022-09-16 17:58:39 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import * as math from "mathjs";
+
+import "./App.css";
+import Button from "./components/Button";
+
+const App = () => {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+
+  const addToText = (val) => {
+    setText((text) => [...text, val]);
+  };
+
+  const calculateResult = () => {
+    const input = text.join(""); // Remove commas
+    console.log(input);
+    setResult(math.evaluate(input));
+  };
+
+  const resetInput = () => {
+    setText("");
+    setResult("");
+  };
+
+  // <Button symbol="7" handleClick={addToText} />;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calc_wrapper">
+        <div className="display_container">
+          <div className="previous">123+</div>
+          <div className="current">4561111111</div>
+        </div>
+        <div className="btn_container">
+          <Button symbol="AC" />
+          <Button symbol="DEL" />
+          <Button symbol="%" />
+          <Button symbol="/" />
+          <Button symbol="7" />
+          <Button symbol="8" />
+          <Button symbol="9" />
+          <Button symbol="*" />
+          <Button symbol="4" />
+          <Button symbol="5" />
+          <Button symbol="6" />
+          <Button symbol="-" />
+          <Button symbol="1" />
+          <Button symbol="2" />
+          <Button symbol="3" />
+          <Button symbol="+" />
+          <Button symbol="0" />
+          <Button symbol="." />
+          <Button symbol="=" btn2="btn2" spanTwo="spanTwo" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import "./Button.css";
+
+const Button = ({
+  symbol = 0,
+  color = "#243264",
+  handleClick,
+  btn2 = "",
+  spanTwo = "",
+}) => {
+  return (
+    <div
+      // onClick={() => handleClick(symbol)}
+      className={"button-wrapper " + btn2 + " " + spanTwo}
+      style={{ backgroundColor: color }}
+    >
+      {symbol}
+    </div>
+  );
+};
+
+export default Button;
